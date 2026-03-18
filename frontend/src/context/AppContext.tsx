5b6c34cb29c553525022170216cfc01d813c72aa
Hashmat <hashmatullah.armani@dci-student.org>
2026-03-18 09:45:26 +0100

    save

--- frontend/src/context/AppContext.tsx
+++ frontend/src/context/AppContext.tsx
@@ -9,10 +9,11 @@ import Cookies from "js-cookie";
 // import { cookies } from "next/headers";
 import axios from "axios";
 
-export const utils_service = "http://localhost:5001";
-export const auth_service = "http://localhost:5000";
-export const user_service = "http://localhost:5002";
-export const job_service = "http://localhost:5003";
+export const utils_service =
+  process.env.UTILS_SERVICE || "http://localhost:5001";
+export const auth_service = process.env.AUTH_SERVICE || "http://localhost:5000";
+export const user_service = process.env.USER_SERVICE || "http://localhost:5002";
+export const job_service = process.env.JOB_SERVICE || "http://localhost:5003";
 
 const AppContext = createContext<AppContextType | undefined>(undefined);
 
